46cbe1fc89c883bb724ea927abbb92340be9f5d3
ishwar2303 <68193554+ishwar2303@users.noreply.github.com>
2020-07-23 00:20:25 +0530

    Update viewshops.php

--- ishwar/zarurat/viewshops.php
+++ ishwar/zarurat/viewshops.php
@@ -1,3 +1,9 @@
+<?php
+	require_once('includes/common.php');
+	$seller_id = $_SESSION['seller_id'];
+	$sql = "SELECT * FROM seller_shop JOIN shop_category on seller_shop.shop_category_id=shop_category.shop_category_id WHERE seller_id='$seller_id'";
+	$result = mysqli_query($con,$sql);
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,75 +17,43 @@
 </head>
 <body>
 	<?php include 'sellerLoggedInHeader.php'; ?>
+
 		<div class="wrapper">
+			<?php 
+			if($result->num_rows>0)
+			{
+			?>
 			<div class="allshops">
+			<?php
+			while($row = $result->fetch_assoc())
+			{
+				?>
+			
 				<div class="shop-details">
-					<div class="shopname">ShopName</div>
-					<div class="shoptype">Grocery</div>
+					<div class="shopname"><?php echo $row['shop_name'];?></div>
+					<div class="shoptype"><?php echo $row['shop_category_name'];?></div>
 					<div class=view>
-						<a href="">View</a>
-						<a href="">Add more items</a>
-					</div>
-				</div>
-				<div class="shop-details">
-					<div class="shopname">ShopName</div>
-					<div class="shoptype">Grocery</div>
-					<div class=view>
-						<a>View</a>
-						<a>Add more items</a>
-					</div>
-				</div>
-				<div class="shop-details">
-					<div class="shopname">ShopName</div>
-					<div class="shoptype">Grocery</div>
-					<div class=view>
-						<a>View</a>
-						<a>Add more items</a>
-					</div>
-				</div>
-				<div class="shop-details">
-					<div class="shopname">ShopName</div>
-					<div class="shoptype">Grocery</div>
-					<div class=view>
-						<a>View</a>
-						<a>Add more items</a>
-					</div>
-				</div>
-				<div class="shop-details">
-					<div class="shopname">ShopName</div>
-					<div class="shoptype">Grocery</div>
-					<div class=view>
-						<a>View</a>
-						<a>Add more items</a>
-					</div>
-				</div>
-				<div class="shop-details">
-					<div class="shopname">ShopName</div>
-					<div class="shoptype">Grocery</div>
-					<div class=view>
-						<a>View</a>
-						<a>Add more items</a>
-					</div>
-				</div>
-				<div class="shop-details">
-					<div class="shopname">ShopName</div>
-					<div class="shoptype">Grocery</div>
-					<div class=view>
-						<a>View</a>
-						<a>Add more items</a>
-					</div>
-				</div>
-				<div class="shop-details">
-					<div class="shopname">ShopName</div>
-					<div class="shoptype">Grocery</div>
-					<div class=view>
-						<a>View</a>
-						<a>Add more items</a>
+						<?php
+						$shopID = base64_encode($row['shop_id']);
+						$shopcategoryID = base64_encode($row['shop_category_id']);
+						?>
+						<a href="viewProductsInShop.php?shopID=<?php echo $shopID; ?>">View Products</a>
+						<a href="product_list.php?shopID=<?php echo $shopID; ?>&shopcategoryID=<?php echo $shopcategoryID; ?>">Add more items</a>
 					</div>
 				</div>
 
-
+				<?php
+			}
+			?>
 			</div>
+		<?php	
+		}
+		else{
+			?>
+			<h1 style="display: flex; justify-content: center; align-items: center; height: 55vh;"class="message">Shop Not Registered</h1>
+			<?php
+		}
+		?>
 		</div>
 	<?php include 'footer.php'; ?>
 </body>
