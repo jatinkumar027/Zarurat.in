1687b1d4522bbee227826c567c9b748f4ae093ae
Jatin Kumar <jatink220@gmail.com>
2020-07-25 21:55:36 +0530

    Updated index etc

--- index.php
+++ index.php
@@ -13,31 +13,29 @@
 		<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/owl.carousel.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
   </head>
   <body>
 		<header class="site-header">
 			<nav>
 				<!-- logo for website, Yet to work -->
 				<div class="logo">
-          <img src="images/Zarurat_logo.png" alt="">
+          <a href="index.php"><img src="images/Zarurat_logo.png" alt="Zarurat.in"></a>
 				</div>
 				<!-- Menu, Yet to attach with other pages -->
 				<div class="menu">
-					<ul>
-						<li>Home</li>
-						<li>login</li>
-						<li>About us</li>
-					</ul>
-				</div>
+					<button class="button-top" onclick="window.open('seller_home.php','_blank');" style="vertical-align:middle"><span>Start Selling</span></button>
+				</div>'
 			</nav>
-			<section>
-				<!-- creating quotes -->
+			<section class='header-section'>
+				<!-- adding image -->
 				<div class="leftside">
-          <div>
             <img src="images/home_screen_demo_mobile.png">
-          </div>
-
-
 				</div>
         <!-- from : https://bbbootstrap.com/snippets/awesome-app-store-buttons-32348296 -->
         <div class="page-content page-container" id="page-content">
@@ -65,50 +63,41 @@
 					<p>Your friend for all your daily requirements <br><br>
           More products arriving soon
           </p>
-					<button style="border-radius:10px;">Buy Now</button>
+					<button class="button" onclick="window.location.href='buyer_home.php';" style="vertical-align:middle;"><span>Buy Now</span></button>
 				</div>
 			</section>
 		</header>
-		<!-- adding browser and search box -->
-		<section class="main_content">
 
-			<!-- adding shop categories -->
-		<div class="shop_container">
+    <!-- ABOUT -->
+    <section id="about" data-stellar-background-ratio="0.5">
+         <div class="container">
+              <div class="row">
 
-			<form class="example search-bar" action="action_page.php">
-				<div>
-					 <input type="text" placeholder="Search.." name="search">
-				</div>
-				<div>
-					 <button type="submit"><i class="fa fa-search"></i></button>
-				</div>
-			</form>
+                   <div class="col-md-6 col-sm-12">
+                        <div class="about-info">
+                             <div class="section-title wow fadeInUp" data-wow-delay="0.2s">
+                                  <h4>Read our story</h4>
+                                  <h2 style="font-weight: bold; font-size:22px;"><em>Zarurat.in is a newborn child with Talent</em> </h2>
+                             </div>
 
-		   <div class="clearfix ">
-		       <div class="shop_search_item ">
-			        <div class="shop_image">
-			        	<a href=""><img style="width: 275px; height: 200px;" src="images/home_page_kirana.jpg">
-			        	 <h3>Kirana Store</h3></a>
-			        </div>
+                             <div class="wow fadeInUp" data-wow-delay="0.4s">
+                                  <p style="font-size:15px;">Situation like Covid-19 may have a greater chance to happen again in future. People suffered a lot while sitting at home and they were abondoned of many things including the necessities.</p>
+                                  <p style="font-size:15px;">Whether Online Payments, or flexiblity to buy at ease, Buy some Medicine or Milk Products, We take care of everything.</p>
+                                  <p style="font-size:15px;">Zarurat.in aims to provide the necessities at your doorstep. No worry to go out of home and taking risks or tension to buy lots of items. We are here to help you and provide the best from your nearby.</br></br><b>Just Log in and say Zarurat.in ;)</b></p>
+                             </div>
+                        </div>
+                   </div>
 
-		       </div>
+                   <div class="col-md-6 col-sm-12">
+                        <div class="wow fadeInUp about-image" data-wow-delay="0.6s" style="float:right;">
+                             <img style="width:550px; height: 500px; border-radius:3px;" src="images/plain-zero-waste-bag-placed-on-marble-table-3850556.jpg" class="img-responsive" alt="">
+                        </div>
+                   </div>
 
-		       <div class="shop_search_item ">
-			        <div class="shop_image">
-			        	<img style="width: 275px; height: 200px;" src="images/home_page_dairy.jpg">
-			        	<a href="#"><h3>Dairy Store</h3></a>
-			        </div>
-		       </div>
+              </div>
+         </div>
+    </section>
 
-		       <div class="shop_search_item">
-			        <div class="shop_image">
-			        	<img style="width: 275px; height: 200px;" src="images/home_page_medical.jpg">
-			        	<a href="#"><h3>Medical Store</h3></a>
-			        </div>
-		       </div>
-	       </div>
-		</div>
-		</section>
 		<!-- Footer Added -->
 		<footer class="contain">
 				<div class="maker">
